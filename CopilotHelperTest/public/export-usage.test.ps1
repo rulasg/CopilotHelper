c9be0d567944870df4d9ae1785cdafcca145bda1
rulasg <rulasg@outlook.com>
2023-12-18 16:51:08 +0100

    mock api call

--- CopilotHelperTest/public/export-usage.test.ps1
+++ CopilotHelperTest/public/export-usage.test.ps1
@@ -1,24 +1,33 @@
 
 function CopilotHelperTest_ExportCopilotUsage_Totals{
 
-    $result = Export-CopilotUsageTotals -Owner solidifydemo -OutputFile  totals.csv
+    $testDataItem = $PSScriptRoot | Join-Path -ChildPath 'testData' -AdditionalChildPath 'CopilotUsageOrg.json' | Get-Item
+    Set-InvokeCommandAlias -Alias CopilotUsage -Command "Get-Content -Path $($testDataItem.FullName)"
+
+    Export-CopilotUsageTotals -Owner solidifydemo -OutputFile totals.csv
 
     Assert-ItemExist -Path totals.csv
 
     $imported = Import-Csv -Path totals.csv
 
-    Assert-NotImplemented
-
+    Assert-Count -Expected 27 -Presented $imported
 }
 
 function CopilotHelperTest_ExportCopilotUsage_Breakdown{
 
-    $result = Export-CopilotUsageBreakdown -Owner solidifydemo -OutputFile breakdown.csv
+    $testDataItem = $PSScriptRoot | Join-Path -ChildPath 'testData' -AdditionalChildPath 'CopilotUsageOrg.json' | Get-Item
+    Set-InvokeCommandAlias -Alias CopilotUsage -Command "Get-Content -Path $($testDataItem.FullName)"
+
+    Export-CopilotUsageBreakdown -Owner solidifydemo -OutputFile breakdown.csv
 
     Assert-ItemExist -Path breakdown.csv
 
     $imported = Import-Csv -Path breakdown.csv
 
-    Assert-NotImplemented
+    Assert-Count -Expected 237 -Presented $imported
+
+    $languages = $imported | Select-Object -Property language -Unique
+
+    Assert-Count -Expected 35 -Presented $languages
 
 }
