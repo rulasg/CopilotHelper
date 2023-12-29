6b2a8d016cd6a0b9201c5cb79323eaf44c534a58
rulasg <rulasg@outlook.com>
2023-12-29 01:00:44 +0100

    fix test Get-CopilotUsage

--- CopilotHelperTest/public/getUsageDiagram.test.ps1
+++ CopilotHelperTest/public/getUsageDiagram.test.ps1
@@ -30,53 +30,100 @@ pie showData
 }
 
 function CopilotHelperTest_GetCopilotUsageDiagramOrg_Breakdown{
-    $users =@'
-``` mermaid
-pie showData
-  title active_users [432]
-  "Other": 103
-  "typescript": 54
-  "markdown": 46
-  "powershell": 44
-  "javascript": 31
-  "json": 25
-  "github-actions-workflow": 22
-  "text": 21
-  "json with comments": 20
-  "c#": 18
-  "python": 17
-  "bicep": 16
-  "yaml": 15
 
-```
+# ``` mermaid
+# pie showData
+#   title active_users [432]
+#   "Other": 103
+#   "typescript": 54
+#   "markdown": 46
+#   "powershell": 44
+#   "javascript": 31
+#   "json": 25
+#   "github-actions-workflow": 22
+#   "text": 21
+#   "json with comments": 20
+#   "c#": 18
+#   "python": 17
+#   "bicep": 16
+#   "yaml": 15
 
-'@
+# ```
 
-    $lines =@'
-``` mermaid
-pie showData
-  title lines_accepted [5855]
-  "powershell": 1574
-  "Other": 1370
-  "javascript": 1002
-  "c#": 886
-  "python": 531
-  "typescript": 492
+# ``` mermaid
+# pie showData
+#   title lines_accepted [5855]
+#   "powershell": 1574
+#   "Other": 1370
+#   "javascript": 1002
+#   "c#": 886
+#   "python": 531
+#   "typescript": 492
 
-```
-
-'@
+# ```
 
     $result = Get-CopilotUsageOrgDiagramBreakdown -Owner someOrgName
 
-    Assert-Contains -Expected $users -Presented $result
-    Assert-Contains -Expected $lines -Presented $result
+    @(
+        # Userts
+        'title active_users [432]'
+        '"Other": 103',
+
+        # Lines
+        'title lines_accepted [5855]',
+        '"Other": 1370'
+
+    ) | ForEach-Object {
+        Assert-IsTrue -Condition $($result[0].Contains($_) -or $result[1].Contains($_))
+    }
+
 }
 
 function CopilotHelperTest_GetCopilotUsageDiagramEnterprise_Breakdown{
 
-    Assert-NotImplemented
+# ``` mermaid
+# pie showData
+#   title active_users [864]
+#   "Other": 206
+#   "typescript": 108
+#   "markdown": 92
+#   "powershell": 88
+#   "javascript": 62
+#   "json": 50
+#   "github-actions-workflow": 44
+#   "text": 42
+#   "json with comments": 40
+#   "c#": 36
+#   "python": 34
+#   "bicep": 32
+#   "yaml": 30
+
+# ```
+
+# ``` mermaid
+# pie showData
+#   title lines_accepted [11710]
+#   "powershell": 3148
+#   "Other": 2740
+#   "javascript": 2004
+#   "c#": 1772
+#   "python": 1062
+#   "typescript": 984
+
+# ```
+
     $result = Get-CopilotUsageEnterpriseDiagramBreakdown -Enterprise someEnterpriseName
 
-    Assert-Count -Expected $($dataResultsBreakdownEnterprise + 2) -Presented $result
+    @(
+        # Userts
+        'title active_users [864]'
+        '"Other": 206',
+
+        # Lines
+        'title lines_accepted [11710]',
+        '"Other": 2740'
+        
+    ) | ForEach-Object {
+        Assert-IsTrue -Condition $($result[0].Contains($_) -or $result[1].Contains($_))
+    }
 }
